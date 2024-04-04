03b73eae67ad48bd50f88bc13c823fd1d542054e
Douglas Tomacheski de Abreu e Silva <dotomasi@outlook.com>
2024-04-04 19:18:17 -0300

    feat(app): new CSS styles for the app
    new CSS styles for the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  /*   {
-      path: 'home',
-      loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-    },
-    {
-      path: '',
-      redirectTo: 'home',
-      pathMatch: 'full'
-    }, */
+  {
+    path: 'vehicles',
+    loadChildren: () => import('./vehicles/vehicles.module').then(m => m.VehiclesPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'vehicles',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
